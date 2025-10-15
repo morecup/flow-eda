fab9fe3c0dad11e18b88ebee9e6e24c89e247116
morecup <morecup@163.com>
2025-10-15 17:25:03 +0800

    更新前端

--- flow-eda-vue/vite.config.js
+++ flow-eda-vue/vite.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'vite'
 import vue from "@vitejs/plugin-vue";
 
-export default {
+export default defineConfig({
   server: {
     port: 3000,
     proxy: {
@@ -16,4 +17,4 @@ export default {
     },
   },
   plugins: [vue()],
-};
+});
