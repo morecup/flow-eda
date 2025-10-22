6da502d827e7f64fef10fd82d6c24a5e6e260802
morecup <morecup@163.com>
2025-10-22 21:05:43 +0800

    feat(runner): FlowExecutor 上报改为使用 instanceId；补充测试与依赖 (refs #2)

--- flow-eda-runner/src/test/java/com/flow/eda/runner/FlowEdaRunnerApplicationTests.java
+++ flow-eda-runner/src/test/java/com/flow/eda/runner/FlowEdaRunnerApplicationTests.java
@@ -1,9 +1,7 @@
 package com.flow.eda.runner;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FlowEdaRunnerApplicationTests {
 
     @Test
