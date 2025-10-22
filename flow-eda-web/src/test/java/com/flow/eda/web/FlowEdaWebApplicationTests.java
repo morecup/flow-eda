9e551e25af269a25c6e55e2e2173a522e92feb5c
morecup <morecup@163.com>
2025-10-22 19:15:21 +0800

    test(web): 修复测试上下文与依赖问题，完成全量构建 (refs #1)

--- flow-eda-web/src/test/java/com/flow/eda/web/FlowEdaWebApplicationTests.java
+++ flow-eda-web/src/test/java/com/flow/eda/web/FlowEdaWebApplicationTests.java
@@ -3,7 +3,7 @@ package com.flow.eda.web;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TestBootApplication.class)
 class FlowEdaWebApplicationTests {
 
     @Test
--- flow-eda-web/src/test/java/com/flow/eda/web/log/LogContentProxyTest.java
+++ flow-eda-web/src/test/java/com/flow/eda/web/log/LogContentProxyTest.java
@@ -1,17 +1,21 @@
 package com.flow.eda.web.log;
 
 import com.flow.eda.common.http.Result;
+import com.flow.eda.web.TestBootApplication;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
+@SpringBootTest(classes = {TestBootApplication.class, LogContentProxyTest.LogTestConfig.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
         "spring.cloud.nacos.discovery.enabled=false",
         "spring.cloud.nacos.config.enabled=false"
 })
@@ -22,6 +26,15 @@ public class LogContentProxyTest {
 
     @MockBean
     private LogClient logClient;
+
+    @TestConfiguration
+    @ComponentScan(basePackages = "com.flow.eda.web.log")
+    static class LogTestConfig {
+        @Bean
+        public com.flow.eda.web.flow.FlowMapper flowMapper() {
+            return org.mockito.Mockito.mock(com.flow.eda.web.flow.FlowMapper.class);
+        }
+    }
 
     @Test
     void should_proxy_log_content() {
